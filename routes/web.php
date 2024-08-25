dde1e8b294e223e39bc611a6630ff004fd87f988
Sarwidad <h1101211052@student.untan.ac.id>
2024-08-25 16:53:13 +0700

    change routes

--- routes/web.php
+++ routes/web.php
@@ -21,62 +21,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/', function () {
     return view('landing');
-});
-
-Route::get('/content', function () {
-    return view('content');
-})->middleware(['auth', 'verified'])->name('content');
-
-Route::middleware('auth')->group(function () {
-    // Route untuk Dashboard
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-
-    Route::get('/dashboardAsli', function () {
-        return view('dashboardAsli');
-    })->name('dashboardAsli');
-
-    // Route untuk Kriteria
-    Route::get('/kriteria', function () {
-        return view('kriteria');
-    })->name('kriteria');
-
-    // Route untuk Alternatif
-    Route::get('/alternatif', function () {
-        return view('alternatif');
-    })->name('alternatif');
-
-    // Route untuk Penilaian
-    Route::get('/penilaian', function () {
-        return view('penilaian');
-    })->name('penilaian');
-
-    // Route untuk ARAS
-    Route::get('/aras', function () {
-        return view('aras');
-    })->name('aras');
-
-    // Route untuk CODAS
-    Route::get('/codas', function () {
-        return view('codas');
-    })->name('codas');
-
-    // Route untuk Uji Sensitivitas
-    Route::get('/uji', function () {
-        return view('uji');
-    })->name('uji');
-
-    // // Route untuk profile
-    // Route::get('/profile', function () {
-    //     return view('profile');
-    // })->name('profile');
 });
 
 Route::middleware('auth')->group(function () {
@@ -90,11 +36,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/codas', [CodasController::class, 'index'])->name('codas');
     Route::get('/uji', [UjiController::class, 'index'])->name('uji');
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-    // Route::get('/uji', [UjiController::class, 'adjustAndNormalizeWeights'])->name('uji');
-    // Route::get('/uji', [UjiController::class, 'ARAS'])->name('uji');
-    // Route::get('/uji', [UjiController::class, 'CODAS'])->name('uji');
-    // Route::get('/uji', [UjiController::class, 'calculateArasRanking'])->name('calculateArasRanking');
-    // Route::get('/uji', [UjiController::class, 'calculateCodasRanking'])->name('calculateCodasRanking');
 });
 
 require __DIR__.'/auth.php';
